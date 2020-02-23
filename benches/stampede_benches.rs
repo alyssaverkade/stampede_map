8b325ded13cba3fc510412e05a94bb508f17faa7
Alyssa Verkade <alyssaverkade@gmail.com>
2020-02-23 13:41:06 -0800

    Rework benchmarks to run faster + give more data
    Previously, while criterion was measuring the hot loop correctly,
    the runtime of the bechmarks were dominated by allocation overhead.
    Subsequently we use the opportunity to augment the existing benchmarks
    with throughput measurements along with making the benchmarks run ~10x
    faster

--- benches/stampede_benches.rs
+++ benches/stampede_benches.rs
@@ -1,41 +1,74 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use rand::distributions::Uniform;
-use rand::Rng;
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 use stampede_map::StampedeMap;
-
-const INSERTION_SIZE: usize = 1_000;
 
 #[global_allocator]
 static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
-pub fn stampede_benches(c: &mut Criterion) {
-    let mut rng = rand::thread_rng();
-    let dist = Uniform::new(0, 1 << 32);
-    let mut values = Vec::with_capacity(INSERTION_SIZE);
-    for _ in 0..INSERTION_SIZE {
-        values.push((
-            rng.sample::<usize, Uniform<usize>>(dist),
-            rng.sample::<usize, Uniform<usize>>(dist),
-        ));
+#[inline(always)]
+fn generate_uniform_values(size: usize) -> Vec<(usize, usize)> {
+    let mut values = vec![(0, 0); size];
+    for i in values.iter_mut() {
+        *i = (rand::random(), rand::random());
     }
-    c.bench_function("random insertion", |b| {
-        let mut map: StampedeMap<_, _> = StampedeMap::with_capacity(1 << 28);
-        b.iter(|| {
-            for (k, v) in &values {
-                black_box(map.set(*k, *v));
-            }
-        })
-    });
-    c.bench_function("random lookup", |b| {
-        let mut map: StampedeMap<_, _> = StampedeMap::with_capacity(1 << 28);
-        for (k, v) in &values {
-            map.set(*k, *v);
-        }
-        b.iter(|| {
-            black_box(map.get(rand::random::<usize>()));
-        })
-    });
+    values
 }
 
-criterion_group!(benches, stampede_benches);
+pub fn lookup_not_existing(c: &mut Criterion) {
+    static KB: usize = 1024;
+    let mut group = c.benchmark_group("get not existing");
+    for size in [16, 128, KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB]
+        .iter()
+        .copied()
+    {
+        let data = generate_uniform_values(size);
+        let mut map: StampedeMap<_, _> = black_box(StampedeMap::with_capacity(size));
+        for (k, v) in &data {
+            black_box(map.set(*k, *v));
+        }
+        group.throughput(Throughput::Bytes(size as u64));
+        group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, _size| {
+            b.iter(|| {
+                black_box(map.get(1));
+            });
+        });
+    }
+}
+
+pub fn lookup_existing(c: &mut Criterion) {
+    static KB: usize = 1024;
+    let mut group = c.benchmark_group("get existing");
+    for size in [KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB].iter().copied() {
+        let data = generate_uniform_values(size);
+        let mut map: StampedeMap<_, _> = StampedeMap::with_capacity(size);
+        for (k, v) in &data {
+            map.set(*k, *v);
+        }
+        map.set(1, 5);
+        group.throughput(Throughput::Bytes(size as u64));
+        group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, _size| {
+            b.iter(|| {
+                black_box(map.get(1));
+            })
+        });
+    }
+}
+
+pub fn insertion(c: &mut Criterion) {
+    static KB: usize = 1024;
+    let mut group = c.benchmark_group("get not existing");
+    for size in [KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB].iter().copied() {
+        let data = generate_uniform_values(size);
+        let mut map: StampedeMap<_, _> = StampedeMap::with_capacity(size);
+        group.throughput(Throughput::Bytes(size as u64));
+        group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, _size| {
+            b.iter(|| {
+                for (k, v) in &data {
+                    map.set(*k, *v);
+                }
+            })
+        });
+    }
+}
+
+criterion_group!(benches, lookup_not_existing, insertion);
 criterion_main!(benches);
